b12cfe8a439154ba84a2c8defc1719d548dad3d6
binoy <binoyav@gmail.com>
2024-08-08 12:07:26 +0530

    Api auth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Invalid credentials', 'data' => $request->all()], 401);
+        }
+
+        // Valid user
+        $user = Auth::user();
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+}
